29d60d375e76242ce6b41ecf876a585764fc4593
Gregory Petit <grpetit@gmail.com>
2019-10-02 16:16:01 +0200

    atelier 1 ok

---
+++ cours_node/cours_node.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.red("wilder de malade"));
+console.log(chalk.yellow("toto"));
+console.log(chalk.blue("riri"));
+console.log(chalk.blue("Hello world!"));
