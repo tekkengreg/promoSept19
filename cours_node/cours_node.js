d8059d623a2d1ff251eab5fc78b80c0ade40f2d1
Gregory Petit <grpetit@gmail.com>
2019-10-02 16:25:11 +0200

    atelier 2 ok

--- cours_node/cours_node.js
+++ cours_node/cours_node.js
@@ -4,3 +4,13 @@ console.log(chalk.red("wilder de malade"));
 console.log(chalk.yellow("toto"));
 console.log(chalk.blue("riri"));
 console.log(chalk.blue("Hello world!"));
+
+const http = require("http");
+const port = 5000;
+const server = http.createServer((request, response) => {
+  response.writeHead(200);
+  response.end("Hello World");
+});
+
+server.listen(port);
+console.log(`Adresse du serveur:http://localhost:${port}`);
